46ac1d7ed94fb2d90c29ecb685e5468c3b1a991b
Eric <eric.m.lombardo@gmail.com>
2021-02-16 17:29:49 -0500

    Build complex forms for pet domain model
    Created nested hashes to allow for assigning multiple associations to
    one object as well as create new objects to associate to that object.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,11 +6,17 @@ class PetsController < ApplicationController
   end
 
   get '/pets/new' do 
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
-  post '/pets' do 
-
+  post '/pets' do # create pet, assign owner, create owner if one
+    if params[:owner][:name].empty?
+     @pet = Pet.create(params[:pet])
+    else
+      owner = Owner.create(params[:owner])
+      @pet = Pet.create(name: params[:pet][:name], owner_id: owner.id)
+    end
     redirect to "pets/#{@pet.id}"
   end
 
@@ -19,8 +25,21 @@ class PetsController < ApplicationController
     erb :'/pets/show'
   end
 
-  patch '/pets/:id' do 
+  get '/pets/:id/edit' do
+    @pet = Pet.find(params[:id])
+    @owners = Owner.all
+    erb :'pets/edit'
+  end
 
+  patch '/pets/:id' do 
+    @pet = Pet.find(params[:id])
+    if params[:owner][:name].empty?
+      @pet.update(params[:pet])
+    else
+      @pet.update(params[:pet]) 
+      @pet.owner_id = Owner.create(params[:owner]).id
+      @pet.save
+    end
     redirect to "pets/#{@pet.id}"
   end
 end
